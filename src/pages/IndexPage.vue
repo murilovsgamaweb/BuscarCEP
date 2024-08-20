ea9b48bc22694bbcef03ab2a0917881d8557a0cc
Murilovsgama <murilo.gold5@gmail.com>
2024-08-20 13:58:02 -0300

    BuscarCEP início

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      
+        <q-input outlined v-model="text" label="CEP" />
+        <q-input outlined readonly v-model="text" label="Cidade" />
+        <q-input outlined readonly v-model="text" label="Estado" />
+        <q-input outlined readonly v-model="text" label="Bairro" />
+        <q-input outlined readonly v-model="text" label="Logradouro" />
+        <q-input outlined v-model="text" label="Complemento" />
+      </div>
+  
+  </div>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
+
+<style scoped>
+
+
+.camposCEP q-input {
+  width: 80%;
+  background: red;
+  border: 5rem solid red;
+  color: red;
+}
+</style>
